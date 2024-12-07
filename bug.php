f51b60a2a7bc6b3e2dd12edb755eade6ba00aa24
BugHunter-X <askzenai@gmail.com>
2024-12-07 10:04:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,16 @@
+```php
+<?php
+function increment_array_value(array &$arr, $key, $increment = 1) {
+  if (!isset($arr[$key])) {
+    $arr[$key] = 0; // Initialize if key doesn't exist
+  }
+  $arr[$key] += $increment; // Increment the value
+}
+
+$my_array = ['a' => 1, 'b' => 2];
+increment_array_value($my_array, 'c', 5);
+print_r($my_array);
+increment_array_value($my_array, 'a');
+print_r($my_array);
+?>
+```
